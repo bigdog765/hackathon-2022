4a14dfe8771f3d7a75acfc9707ab65fcf7ef66a8
Pollchowdhury <pollchowdhury7@gmail.com>
2022-02-06 12:27:35 -0500

    fixed ovelapping

--- hackathon.js
+++ hackathon.js
@@ -1,33 +1,27 @@
-const text=document.getElementById("quote");
-const author=document.getElementById("author");
+const text = document.getElementById("quote");
+const author = document.getElementById("author");
 
+const getNewQuote = async () => {
+  //api for quotes
+  var url = "https://type.fit/api/quotes";
 
-const getNewQuote = async () =>
-{
-    //api for quotes
-    var url="https://type.fit/api/quotes";    
+  // fetch the data from api
+  const response = await fetch(url);
+  console.log(typeof response);
+  //convert response to json and store it in quotes array
+  const allQuotes = await response.json();
 
-    // fetch the data from api
-    const response=await fetch(url);
-    console.log(typeof response);
-    //convert response to json and store it in quotes array
-    const allQuotes = await response.json();
+  const indx = Math.floor(Math.random() * allQuotes.length);
 
-    const indx = Math.floor(Math.random()*allQuotes.length);
+  const quote = allQuotes[indx].text;
+  const auth = allQuotes[indx].author;
 
- 
-    const quote=allQuotes[indx].text;
-    const auth=allQuotes[indx].author;
+  if (auth == null) {
+    author = "Anonymous";
+  }
 
-    if(auth==null)
-    {
-        author = "Anonymous";
-    }
-
-    //function to dynamically display the quote and the author
-    text.innerHTML=quote;
-    author.innerHTML="~ "+auth;
-
-    
-}
+  //function to dynamically display the quote and the author
+  text.innerHTML = quote;
+  author.innerHTML = "~ " + auth;
+};
 getNewQuote();
